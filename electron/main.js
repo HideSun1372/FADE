91f86f43b38662e21171a5530a8efa8eb43c60a8
Nandery <siqichen0802@gmail.com>
2026-05-24 21:53:03 -0700

    Higher buffer for warning message

--- electron/main.js
+++ electron/main.js
@@ -29,21 +29,16 @@ app.whenReady().then(() => {
 
   mainWindow.loadFile('loading.html');
   mainWindow.on('page-title-updated', (e) => e.preventDefault());
-  mainWindow.webContents.on('before-input-event', (event, input) => {
-    if (input.key === 'F12') mainWindow.webContents.toggleDevTools();
-  });
+
 
   setTimeout(() => {
     if (mainWindow) mainWindow.webContents.executeJavaScript(
       'document.getElementById("error-msg").style.display = "block"'
     );
-  }, 30000);
+  }, 180000);
 
   waitForBackend(2000, () => {
-    if (mainWindow) {
-      mainWindow.loadURL(BACKEND_URL);
-      mainWindow.webContents.openDevTools();
-    }
+    if (mainWindow) mainWindow.loadURL(BACKEND_URL);
   });
 
   mainWindow.on('closed', () => {
